b27d5b119144db981bc21a5e406e1eb2a1f54a54
noarea147 <95benmbarek@gmail.com>
2023-08-03 00:46:36 +0100

    progress

--- src/app/page.js
+++ src/app/page.js
@@ -12,12 +12,39 @@ import LocalMallOutlinedIcon from "@mui/icons-material/LocalMallOutlined";
 import AddCircleOutlineRoundedIcon from "@mui/icons-material/AddCircleOutlineRounded";
 import RemoveCircleOutlineRoundedIcon from "@mui/icons-material/RemoveCircleOutlineRounded";
 import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import AcUnitOutlinedIcon from "@mui/icons-material/AcUnitOutlined";
 
 export default function Home() {
   const [showCoffees, setShowCoffees] = useState(false);
   const [showSmoothies, setShowSmoothies] = useState(false);
   const [showJuices, setShowJuices] = useState(false);
-  const [value, setValue] = React.useState(0);
+  const [value, setValue] = useState(0);
+  const [showModal, setShowModal] = useState(false);
+
+  const handleChange = (pressed) => {
+    switch (pressed) {
+      case "coffee":
+        setShowCoffees(!showCoffees);
+        setShowSmoothies(false);
+        setShowJuices(false);
+        break;
+      case "smoothie":
+        setShowSmoothies(!showSmoothies);
+        setShowCoffees(false);
+        setShowJuices(false);
+        break;
+      case "juice":
+        setShowJuices(!showJuices);
+        setShowCoffees(false);
+        setShowSmoothies(false);
+        break;
+      default:
+        setShowCoffees(false);
+        setShowSmoothies(false);
+        setShowJuices(false);
+        break;
+    }
+  };
 
   const data = [
     { name: "Espresso", price: 2.0 },
@@ -62,10 +89,74 @@ export default function Home() {
           <h2 className="text-2xl font-bold text-center mt-10 mb-5">
             {"Menu"}
           </h2>
+          {/* modal start code */}
+
+          <button
+            className="bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+            type="button"
+            onClick={() => setShowModal(true)}>
+            Open regular modal
+          </button>
+          {showModal ? (
+            <>
+              <div
+              data-aos="fade-up"
+              data-ease-in="ease-in-out"
+              data-aos-duration="500"
+
+              className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none m-4">
+                <div className="relative w-auto my-6 mx-auto max-w-3xl">
+                  {/*content*/}
+                  <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                    {/*header*/}
+                    <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+                      <h3 className="text-3xl font-semibold">Modal Title</h3>
+                      <button
+                        className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                        onClick={() => setShowModal(false)}>
+                        <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                          ×
+                        </span>
+                      </button>
+                    </div>
+                    {/*body*/}
+                    <div className="relative p-6 flex-auto">
+                      <p className="my-4 text-slate-500 text-lg leading-relaxed">
+                        I always felt like I could do anything. That’s the main
+                        thing people are controlled by! Thoughts- their
+                        perception of themselves! They're slowed down by their
+                        perception of themselves. If you're taught you can’t do
+                        anything, you won’t do anything. I was taught I could do
+                        everything.
+                      </p>
+                    </div>
+                    {/*footer*/}
+                    <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+                      <button
+                        className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                        type="button"
+                        onClick={() => setShowModal(false)}>
+                        Close
+                      </button>
+                      <button
+                        className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                        type="button"
+                        onClick={() => setShowModal(false)}>
+                        Save Changes
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+            </>
+          ) : null}
+
+          {/* modal end code */}
         </div>
         <div className="flex flex-col items-center justify-center w-full p-2">
           <div
-            onClick={() => setShowCoffees(!showCoffees)}
+            onClick={() => handleChange("coffee")}
             className="flex flex-row justify-between items-center w-full rounded-xl shadow-xl p-3"
             style={{ backgroundColor: "#ef9336" }}>
             <h3 className="font-bold text-left text-2xl text-white">
@@ -81,7 +172,11 @@ export default function Home() {
           {showCoffees && (
             <div
               className="flex flex-col items-center justify-start w-full pt-3 rounded-b-xl"
-              style={{ backgroundColor: "#ef9336", marginTop: "-10px" }}
+              style={{
+                backgroundColor: "#ef9336",
+                marginTop: "-10px",
+                zIndex: -1,
+              }}
               data-aos="fade-down"
               data-aos-duration="300">
               {data.map((drink) => (
@@ -104,7 +199,7 @@ export default function Home() {
         </div>
         <div className="flex flex-col items-center justify-center w-full p-2">
           <div
-            onClick={() => setShowSmoothies(!showSmoothies)}
+            onClick={() => handleChange("smoothie")}
             className="flex flex-row justify-between items-center w-full rounded-xl shadow-xl p-3"
             style={{ backgroundColor: "#ef9336" }}>
             <h3 className="font-bold text-left text-2xl text-white">
@@ -122,7 +217,11 @@ export default function Home() {
           {showSmoothies && (
             <div
               className="flex flex-col items-center justify-start w-full pt-3 rounded-b-xl"
-              style={{ backgroundColor: "#ef9336", marginTop: "-10px" }}
+              style={{
+                backgroundColor: "#ef9336",
+                marginTop: "-10px",
+                zIndex: -1,
+              }}
               data-aos="fade-down"
               data-aos-duration="300">
               {data.map((drink) => (
@@ -144,7 +243,7 @@ export default function Home() {
           className="flex flex-col items-center justify-center w-full p-2"
           style={{ marginBottom: "74px" }}>
           <div
-            onClick={() => setShowJuices(!showJuices)}
+            onClick={() => handleChange("juice")}
             className="flex flex-row justify-between items-center w-full rounded-xl shadow-xl p-3"
             style={{ backgroundColor: "#ef9336" }}>
             <h3 className="font-bold text-left text-2xl text-white">
@@ -160,7 +259,11 @@ export default function Home() {
           {showJuices && (
             <div
               className="flex flex-col items-center justify-start w-full pt-3 rounded-b-xl"
-              style={{ backgroundColor: "#ef9336", marginTop: "-10px" }}
+              style={{
+                backgroundColor: "#ef9336",
+                marginTop: "-10px",
+                zIndex: -1,
+              }}
               data-aos="fade-down"
               data-aos-duration="300">
               {data.map((drink) => (
