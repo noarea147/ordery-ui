f6f2f19e2ecb222b14b7f15fe94d5c096ba40be6
noarea147 <95benmbarek@gmail.com>
2023-08-07 18:23:27 +0100

    className=

--- src/app/page.js
+++ src/app/page.js
@@ -158,7 +158,7 @@ export default function Home() {
     <div>
       <div className="flex justify-end p-4 w-full">
         <img
-          class="inline-block h-10 w-10 rounded-full ring-2 ring-white"
+          className="inline-block h-10 w-10 rounded-full ring-2 ring-white"
           src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
           alt=""
         />
@@ -167,7 +167,7 @@ export default function Home() {
         className="flex min-h-screen flex-col items-center justify-start"
         data-aos="fade-up"
         data-aos-duration="500">
-        <div class="flex -space-x-2 overflow-hidden"></div>
+        <div className="flex -space-x-2 overflow-hidden"></div>
         <div className="flex flex-col items-center justify-center w-full mt-10 mb-10">
           <Image
             src="/next.svg"
